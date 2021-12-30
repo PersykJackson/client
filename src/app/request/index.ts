8455d676276f73bb1ebf4ec8561502633c45481a
Vladyslav <persyk32111@gmail.com>
2021-12-30 17:40:28 +0200

    Add token to requests

--- src/app/request/index.ts
+++ src/app/request/index.ts
@@ -1,9 +1,22 @@
 import axios from 'axios';
+import store from '../store';
 
 type Data = Record<string, unknown>;
 
 export default axios;
 
-export const post = (url: string, data: Data): Promise<Data> => axios.post(url, data);
+const { token } = store.getState().auth;
 
-export const get = (url: string): Promise<Data> => axios.get(url);
+export const post = (url: string, data: Data): Promise<Data> =>
+  axios.post(url, data, {
+    headers: {
+      authorization: token || '',
+    },
+  });
+
+export const get = (url: string): Promise<Data> =>
+  axios.get(url, {
+    headers: {
+      authorization: token || '',
+    },
+  });
